e679632c66a96219d1a66ae0edf94eed037baccd
Masato Ota <ohtam@nec.com>
2024-11-29 14:35:14 +0900

    update

--- bin/cdk-cross-stack-reference1.ts
+++ bin/cdk-cross-stack-reference1.ts
@@ -5,7 +5,6 @@ import { Stack1 } from "../lib/stack1";
 import { Stack2 } from "../lib/stack2";
 
 const app = new cdk.App();
-// スタックグループにもできるけど、今回はしない
-const stack1 = new Stack1(app, `${projectName}-stack1`);
-const stack2 = new Stack2(app, `${projectName}-stack2`);
+const stack1 = new Stack1(app, "stack1", { stackName: `${projectName}-stack1` });
+const stack2 = new Stack2(app, "stack2", { stackName: `${projectName}-stack2` });
 stack2.addDependency(stack1);
